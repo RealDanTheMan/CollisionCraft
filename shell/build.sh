140ec16fdf2d6876e7aeb7f5b87e0e407a783d1f
RealDanTheMan <dantheman.py@gmail.com>
2025-04-27 14:42:11 +0100

    CMake build script will now produce debug symbols by default

--- shell/build.sh
+++ shell/build.sh
@@ -5,7 +5,7 @@ rm -rf $BUILD_PATH
 mkdir $BUILD_PATH
 mkdir $BUILD_PATH/bin
 
-cmake -B$BUILD_PATH -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake -B$BUILD_PATH -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug
 cmake --build $BUILD_PATH -- -j$(sysctl -n hw.ncpu)
 
 cd $CWD

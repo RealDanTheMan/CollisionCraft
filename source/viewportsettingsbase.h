b63145b276db8934e204485113f88de9d723facb
RealDanTheMan <dantheman.py@gmail.com>
2025-05-01 20:00:58 +0100

    Adding base template for viewport settings panel widget

---
+++ source/viewportsettingsbase.h
@@ -0,0 +1,188 @@
+/********************************************************************************
+** Form generated from reading UI file 'viewport_settingsSJltaX.ui'
+**
+** Created by: Qt User Interface Compiler version 6.8.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef VIEWPORT_SETTINGSSJLTAX_H
+#define VIEWPORT_SETTINGSSJLTAX_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QCheckBox>
+#include <QtWidgets/QFrame>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_ViewportSettingsBase
+{
+public:
+    QVBoxLayout *verticalLayout;
+    QFrame *main_frame;
+    QVBoxLayout *verticalLayout_2;
+    QFrame *model_frame;
+    QVBoxLayout *verticalLayout_3;
+    QLabel *label;
+    QCheckBox *checkBox_2;
+    QCheckBox *checkBox_3;
+    QCheckBox *checkBox;
+    QFrame *collision_frame;
+    QVBoxLayout *verticalLayout_4;
+    QLabel *label_2;
+    QCheckBox *checkBox_4;
+    QCheckBox *checkBox_5;
+    QFrame *button_frame;
+    QHBoxLayout *horizontalLayout;
+    QPushButton *apply_button;
+    QPushButton *cancel_button;
+
+    void setupUi(QWidget *ViewportSettingsBase)
+    {
+        if (ViewportSettingsBase->objectName().isEmpty())
+            ViewportSettingsBase->setObjectName("ViewportSettingsBase");
+        ViewportSettingsBase->resize(330, 335);
+        QSizePolicy sizePolicy(QSizePolicy::Policy::Preferred, QSizePolicy::Policy::Preferred);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(ViewportSettingsBase->sizePolicy().hasHeightForWidth());
+        ViewportSettingsBase->setSizePolicy(sizePolicy);
+        ViewportSettingsBase->setAutoFillBackground(false);
+        verticalLayout = new QVBoxLayout(ViewportSettingsBase);
+        verticalLayout->setObjectName("verticalLayout");
+        verticalLayout->setContentsMargins(4, 4, 4, 4);
+        main_frame = new QFrame(ViewportSettingsBase);
+        main_frame->setObjectName("main_frame");
+        QSizePolicy sizePolicy1(QSizePolicy::Policy::Preferred, QSizePolicy::Policy::Maximum);
+        sizePolicy1.setHorizontalStretch(0);
+        sizePolicy1.setVerticalStretch(0);
+        sizePolicy1.setHeightForWidth(main_frame->sizePolicy().hasHeightForWidth());
+        main_frame->setSizePolicy(sizePolicy1);
+        main_frame->setFrameShape(QFrame::Shape::StyledPanel);
+        main_frame->setFrameShadow(QFrame::Shadow::Raised);
+        verticalLayout_2 = new QVBoxLayout(main_frame);
+        verticalLayout_2->setObjectName("verticalLayout_2");
+        model_frame = new QFrame(main_frame);
+        model_frame->setObjectName("model_frame");
+        sizePolicy1.setHeightForWidth(model_frame->sizePolicy().hasHeightForWidth());
+        model_frame->setSizePolicy(sizePolicy1);
+        model_frame->setFrameShape(QFrame::Shape::StyledPanel);
+        model_frame->setFrameShadow(QFrame::Shadow::Raised);
+        verticalLayout_3 = new QVBoxLayout(model_frame);
+        verticalLayout_3->setSpacing(12);
+        verticalLayout_3->setObjectName("verticalLayout_3");
+        verticalLayout_3->setSizeConstraint(QLayout::SizeConstraint::SetDefaultConstraint);
+        verticalLayout_3->setContentsMargins(4, 4, 4, 32);
+        label = new QLabel(model_frame);
+        label->setObjectName("label");
+        QFont font;
+        font.setBold(true);
+        label->setFont(font);
+
+        verticalLayout_3->addWidget(label);
+
+        checkBox_2 = new QCheckBox(model_frame);
+        checkBox_2->setObjectName("checkBox_2");
+
+        verticalLayout_3->addWidget(checkBox_2);
+
+        checkBox_3 = new QCheckBox(model_frame);
+        checkBox_3->setObjectName("checkBox_3");
+
+        verticalLayout_3->addWidget(checkBox_3);
+
+        checkBox = new QCheckBox(model_frame);
+        checkBox->setObjectName("checkBox");
+
+        verticalLayout_3->addWidget(checkBox);
+
+
+        verticalLayout_2->addWidget(model_frame);
+
+        collision_frame = new QFrame(main_frame);
+        collision_frame->setObjectName("collision_frame");
+        sizePolicy1.setHeightForWidth(collision_frame->sizePolicy().hasHeightForWidth());
+        collision_frame->setSizePolicy(sizePolicy1);
+        collision_frame->setFrameShape(QFrame::Shape::StyledPanel);
+        collision_frame->setFrameShadow(QFrame::Shadow::Raised);
+        verticalLayout_4 = new QVBoxLayout(collision_frame);
+        verticalLayout_4->setObjectName("verticalLayout_4");
+        verticalLayout_4->setContentsMargins(4, 4, 4, 32);
+        label_2 = new QLabel(collision_frame);
+        label_2->setObjectName("label_2");
+        label_2->setFont(font);
+
+        verticalLayout_4->addWidget(label_2);
+
+        checkBox_4 = new QCheckBox(collision_frame);
+        checkBox_4->setObjectName("checkBox_4");
+
+        verticalLayout_4->addWidget(checkBox_4);
+
+        checkBox_5 = new QCheckBox(collision_frame);
+        checkBox_5->setObjectName("checkBox_5");
+
+        verticalLayout_4->addWidget(checkBox_5);
+
+
+        verticalLayout_2->addWidget(collision_frame);
+
+        button_frame = new QFrame(main_frame);
+        button_frame->setObjectName("button_frame");
+        sizePolicy1.setHeightForWidth(button_frame->sizePolicy().hasHeightForWidth());
+        button_frame->setSizePolicy(sizePolicy1);
+        button_frame->setFrameShape(QFrame::Shape::NoFrame);
+        button_frame->setFrameShadow(QFrame::Shadow::Raised);
+        horizontalLayout = new QHBoxLayout(button_frame);
+        horizontalLayout->setObjectName("horizontalLayout");
+        apply_button = new QPushButton(button_frame);
+        apply_button->setObjectName("apply_button");
+
+        horizontalLayout->addWidget(apply_button);
+
+        cancel_button = new QPushButton(button_frame);
+        cancel_button->setObjectName("cancel_button");
+
+        horizontalLayout->addWidget(cancel_button);
+
+
+        verticalLayout_2->addWidget(button_frame);
+
+
+        verticalLayout->addWidget(main_frame);
+
+
+        retranslateUi(ViewportSettingsBase);
+
+        QMetaObject::connectSlotsByName(ViewportSettingsBase);
+    } // setupUi
+
+    void retranslateUi(QWidget *ViewportSettingsBase)
+    {
+        ViewportSettingsBase->setWindowTitle(QCoreApplication::translate("ViewportSettingsBase", "Form", nullptr));
+        label->setText(QCoreApplication::translate("ViewportSettingsBase", "Model Rendering", nullptr));
+        checkBox_2->setText(QCoreApplication::translate("ViewportSettingsBase", "Shaded", nullptr));
+        checkBox_3->setText(QCoreApplication::translate("ViewportSettingsBase", "Lighting", nullptr));
+        checkBox->setText(QCoreApplication::translate("ViewportSettingsBase", "Wireframe", nullptr));
+        label_2->setText(QCoreApplication::translate("ViewportSettingsBase", "Collision Rendering", nullptr));
+        checkBox_4->setText(QCoreApplication::translate("ViewportSettingsBase", "Shaded", nullptr));
+        checkBox_5->setText(QCoreApplication::translate("ViewportSettingsBase", "Wireframe", nullptr));
+        apply_button->setText(QCoreApplication::translate("ViewportSettingsBase", "Apply", nullptr));
+        cancel_button->setText(QCoreApplication::translate("ViewportSettingsBase", "Cancel", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class ViewportSettingsBase: public Ui_ViewportSettingsBase {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // VIEWPORT_SETTINGSSJLTAX_H

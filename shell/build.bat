9596437da7828a377defcb5a607fcaabc45df432
mrdantheman <dantheman.py@gmail.com>
2025-05-30 20:08:54 +0100

    Adding windows build script

---
+++ shell/build.bat
@@ -0,0 +1,15 @@
+echo off
+setlocal
+
+set CWD=%CD%
+set BUILD_PATH=%CWD%build-win
+
+rmdir /s /q %BUILD_PATH%
+mkdir %BUILD_PATH%
+mkdir %BUILD_PATH%/bin
+
+cmake -B%BUILD_PATH% -S . -DCMAKE_TOOLCHAIN_FILE=C:\dev\packages\vcpkg\vcpkg\scripts\buildsystems\vcpkg.cmake -G "Visual Studio 17 2022" -A x64 -DCMAKE_BUILD_TYPE=Release
+cmake --build %BUILD_PATH% --config Release -- /verbosity:detailed
+
+cd %CWD%
+endlocal

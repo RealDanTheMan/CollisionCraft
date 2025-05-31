7b0d7bdfb4d652612809f6df5cfa57a6b0d8c860
mrdantheman <dantheman.py@gmail.com>
2025-05-31 12:03:46 +0100

    Updated windows build script

--- shell/build.bat
+++ shell/build.bat
@@ -2,14 +2,15 @@ echo off
 setlocal
 
 set CWD=%CD%
-set BUILD_PATH=%CWD%build-win
+set BUILD_PATH="%CWD%/build"
+echo "Build Path: %BUILD_PATH%"
 
 rmdir /s /q %BUILD_PATH%
 mkdir %BUILD_PATH%
 mkdir %BUILD_PATH%/bin
 
 cmake -B%BUILD_PATH% -S . -DCMAKE_TOOLCHAIN_FILE=C:\dev\packages\vcpkg\vcpkg\scripts\buildsystems\vcpkg.cmake -G "Visual Studio 17 2022" -A x64 -DCMAKE_BUILD_TYPE=Release
-cmake --build %BUILD_PATH% --config Release -- /verbosity:detailed
+cmake --build %BUILD_PATH% --config Release
 
 cd %CWD%
 endlocal

406c20cc0cf1a0de32537ed04e001adfc3ca6c40
mrdantheman <dantheman.py@gmail.com>
2025-05-31 23:39:15 +0100

    Windows required plugins are not deployed when building on windows

--- shell/build.bat
+++ shell/build.bat
@@ -7,9 +7,16 @@ echo "Build Path: %BUILD_PATH%"
 
 rmdir /s /q %BUILD_PATH%
 mkdir %BUILD_PATH%
-mkdir %BUILD_PATH%/bin
+mkdir %BUILD_PATH%\bin\Release
 
-cmake -B%BUILD_PATH% -S . -DCMAKE_TOOLCHAIN_FILE=C:\dev\packages\vcpkg\vcpkg\scripts\buildsystems\vcpkg.cmake -G "Visual Studio 17 2022" -A x64 -DCMAKE_BUILD_TYPE=Release
+set VCPKG_PATH="C:\dev\packages\vcpkg\vcpkg"
+set VCPKG_TOOLCHAIN="%VCPKG_PATH%\scripts\buildsystems\vcpkg.cmake"
+echo "Deploying plug-ins -> %BUILD_PATH%\bin\Release"
+robocopy "%VCPKG_PATH%\installed\x64-windows\Qt6\plugins\platforms" "%BUILD_PATH%\bin\Release" "qwindows.dll"
+robocopy "%VCPKG_PATH%\installed\x64-windows\lib\usd" "%BUILD_PATH%\bin\Release\usd" "plugInfo.json"
+robocopy "%VCPKG_PATH%\installed\x64-windows\bin\usd" "%BUILD_PATH%\bin\Release\usd" /E
+
+cmake -B%BUILD_PATH% -S . -DCMAKE_TOOLCHAIN_FILE=%VCPKG_TOOLCHAIN% -G "Visual Studio 17 2022" -A x64 -DCMAKE_BUILD_TYPE=Release
 cmake --build %BUILD_PATH% --config Release
 
 cd %CWD%

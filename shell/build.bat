181d35bfab7157e8e32a3e24d79da7b6a21140cc
mrdantheman <dantheman.py@gmail.com>
2025-07-27 19:05:54 +0100

    Updated windows build script to fix dependency resolution across plugins

--- shell/build.bat
+++ shell/build.bat
@@ -19,11 +19,26 @@ if "%VCPKG_PATH%" == "" (
     exit /b 1
 )
 
+REM Run CMake build using VS2017 flavour
 set VCPKG_TOOLCHAIN="%VCPKG_PATH%\scripts\buildsystems\vcpkg.cmake"
-echo "Deploying plug-ins -> %BUILD_PATH%\bin\Release"
-robocopy "%VCPKG_PATH%\installed\x64-windows\Qt6\plugins\platforms" "%BUILD_PATH%\bin\Release" "qwindows.dll"
-robocopy "%VCPKG_PATH%\installed\x64-windows\lib\usd" "%BUILD_PATH%\bin\Release\usd" "plugInfo.json"
-robocopy "%VCPKG_PATH%\installed\x64-windows\bin\usd" "%BUILD_PATH%\bin\Release\usd" /E
+cmake -B%BUILD_PATH% -S . -DCMAKE_TOOLCHAIN_FILE=%VCPKG_TOOLCHAIN% -G "Visual Studio 17 2022" -A x64 -DCMAKE_BUILD_TYPE=Release
+cmake --build %BUILD_PATH% --config Release
+
+set QT_PLUG_SRC="%VCPKG_PATH%\installed\x64-windows\Qt6\plugins"
+set QT_PLUG_DST="%BUILD_PATH%\bin\Release\qt\plugins"
+echo "Deploying QT plug-ins -> %QT_PLUG_DST%"
+
+
+
+xcopy /E /I /Y "%QT_PLUG_SRC%\imageformats" "%QT_PLUG_DST%\imageformats"
+xcopy /E /I /Y "%QT_PLUG_SRC%\platforms" "%QT_PLUG_DST%\platforms"
+xcopy /E /I /Y "%QT_PLUG_SRC%\styles" "%QT_PLUG_DST%\styles"
+
+set USD_PLUG_SRC="%VCPKG_PATH%\installed\x64-windows\bin\usd"
+set USD_PLUG_DST="%BUILD_PATH%\bin\Release\usd"
+echo "Deploying USD plug-ins -> %USD_PLUG_DST%"
+xcopy /E /I /Y "%USD_PLUG_SRC%" "%USD_PLUG_DST%"
+xcopy /Y "%VCPKG_PATH%\installed\x64-windows\lib\usd\plugInfo.json" "%USD_PLUG_DST%"
 
 cmake -B%BUILD_PATH% -S . -DCMAKE_TOOLCHAIN_FILE=%VCPKG_TOOLCHAIN% -G "Visual Studio 17 2022" -A x64 -DCMAKE_BUILD_TYPE=Release
 cmake --build %BUILD_PATH% --config Release
